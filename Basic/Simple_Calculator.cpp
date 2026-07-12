87ad14414eb95e9fccebe643109e0026a8606b09
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-12 23:05:53 +0530

    Add Simple Calculator program in C++

---
+++ Basic/Simple_Calculator.cpp
@@ -0,0 +1,19 @@
+// Question: Write a C++ program to perform addition, subtraction, multiplication, and division.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    cout << "Addition = " << a + b << endl;
+    cout << "Subtraction = " << a - b << endl;
+    cout << "Multiplication = " << a * b << endl;
+    cout << "Division = " << a / b << endl;
+
+    return 0;
+}
