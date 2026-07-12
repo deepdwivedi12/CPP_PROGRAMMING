9d78322aaca58dc09c8b56fb6760f254135e46a6
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-12 23:02:11 +0530

    Add program to input and display an integer

---
+++ Basic/Input_Integer.cpp
@@ -0,0 +1,16 @@
+// Question: Write a C++ program to input and display an integer.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout << "Number = " << num << endl;
+
+    return 0;
+}
