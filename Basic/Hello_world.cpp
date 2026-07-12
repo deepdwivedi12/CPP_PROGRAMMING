bca13cd4d8267ac5a4658a17c2ec925a0b39bf2a
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-12 22:54:27 +0530

    Fix missing newline after output in Hello_world.cpp
    Added a newline after printing 'Hello World!'

--- Basic/Hello_world.cpp
+++ Basic/Hello_world.cpp
@@ -1,8 +1,11 @@
+// Question: Write a C++ program to print "Hello World!".
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!";
+    cout << "Hello World!" << endl;
+
     return 0;
 }
