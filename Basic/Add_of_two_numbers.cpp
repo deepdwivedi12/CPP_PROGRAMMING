9c1b6f9e2e8ee8012d458a550d450131c6ba821f
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-12 23:05:21 +0530

    Create  Add_of_two_numbers.cpp

---
+++ Basic/Add_of_two_numbers.cpp
@@ -0,0 +1,16 @@
+// Question: Write a C++ program to add two numbers.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    cout << "Sum = " << a + b << endl;
+
+    return 0;
+}
