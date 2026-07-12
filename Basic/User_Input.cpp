7ccb34625aebd0fd2198384cc60d39e7aee395e7
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-12 23:00:40 +0530

    Add program to input and display user name

---
+++ Basic/User_Input.cpp
@@ -0,0 +1,16 @@
+// Question: Write a C++ program to input and display your name.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "Your name is " << name << endl;
+
+    return 0;
+}
