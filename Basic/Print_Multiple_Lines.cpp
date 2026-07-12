1992bdea692e15e57006ec878f37d82f99d1a6ea
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-12 22:58:42 +0530

    Add C++ program to print name, college, and branch

---
+++ Basic/Print_Multiple_Lines.cpp
@@ -0,0 +1,13 @@
+// Question: Write a C++ program to print your name, college, and branch.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Name : Deep Dwivedi" << endl;
+    cout << "College : SAGE University" << endl;
+    cout << "Branch : CSE (Data Analytics)" << endl;
+
+    return 0;
+}
