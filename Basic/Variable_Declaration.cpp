514ff96cae2f0f014ca99069b072eae82586553e
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-12 22:59:22 +0530

    Create Variable_Declaration.cpp

---
+++ Basic/Variable_Declaration.cpp
@@ -0,0 +1,13 @@
+// Question: Write a C++ program to declare and print an integer variable.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 18;
+
+    cout << "Age = " << age << endl;
+
+    return 0;
+}
