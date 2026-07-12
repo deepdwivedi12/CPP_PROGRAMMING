edac270a2502ff04684f8baecabd5a2b1ab86650
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-12 23:00:02 +0530

    Add C++ program to declare and print variables
    This program declares and prints different data types including int, float, char, and string.

---
+++ Basic/Multiple_Variables.cpp
@@ -0,0 +1,19 @@
+// Question: Write a C++ program to declare and print different data types.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 18;
+    float marks = 89.5;
+    char grade = 'A';
+    string name = "Deep";
+
+    cout << "Name : " << name << endl;
+    cout << "Age : " << age << endl;
+    cout << "Marks : " << marks << endl;
+    cout << "Grade : " << grade << endl;
+
+    return 0;
+}
