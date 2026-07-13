f94c5422bc96da53c825ca03ccf3d58ff6bfdda8
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-13 22:41:46 +0530

    Create Decrement_Operator.cpp

---
+++ Operators/Decrement_Operator.cpp
@@ -0,0 +1,17 @@
+// Question: Write a C++ program to demonstrate the decrement operator.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+
+    cout << "Before Decrement = " << a << endl;
+
+    a--;
+
+    cout << "After Decrement = " << a << endl;
+
+    return 0;
+}
