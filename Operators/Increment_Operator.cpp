b16420c73161a8cb0226f91184238d412394d742
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-13 22:40:26 +0530

    Create Increment_Operator.cpp

---
+++ Operators/Increment_Operator.cpp
@@ -0,0 +1,17 @@
+// Question: Write a C++ program to demonstrate the increment operator.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+
+    cout << "Before Increment = " << a << endl;
+
+    a++;
+
+    cout << "After Increment = " << a << endl;
+
+    return 0;
+}
