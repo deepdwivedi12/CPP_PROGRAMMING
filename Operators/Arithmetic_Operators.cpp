2acc7be34458f843a5d64eabb9cfd0842984350f
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-13 22:39:52 +0530

    Create Arithmetic_Operators.cpp

---
+++ Operators/Arithmetic_Operators.cpp
@@ -0,0 +1,20 @@
+// Question: Write a C++ program to perform arithmetic operations on two numbers.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    cout << "Addition = " << a + b << endl;
+    cout << "Subtraction = " << a - b << endl;
+    cout << "Multiplication = " << a * b << endl;
+    cout << "Division = " << a / b << endl;
+    cout << "Modulus = " << a % b << endl;
+
+    return 0;
+}
