d9b487b881f27680d194cf47a9a37100353522c2
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-13 22:42:18 +0530

    Add C++ program to demonstrate relational operators

---
+++ Operators/Relational_Operators.cpp
@@ -0,0 +1,18 @@
+// Question: Write a C++ program to demonstrate relational operators.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10, b = 20;
+
+    cout << (a == b) << endl;
+    cout << (a != b) << endl;
+    cout << (a > b) << endl;
+    cout << (a < b) << endl;
+    cout << (a >= b) << endl;
+    cout << (a <= b) << endl;
+
+    return 0;
+}
