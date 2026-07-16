7c81f38a59a7c01d40273481f7e04846f9212bba
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-16 23:45:47 +0530

    Create Print_Numbers_from_10_to_1.cpp

---
+++ Loops/Print_Numbers_from_10_to_1.cpp
@@ -0,0 +1,14 @@
+// Question: Write a C++ program to print numbers from 10 to 1 using for loop.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 10; i >= 1; i--)
+    {
+        cout << i << endl;
+    }
+
+    return 0;
+}
