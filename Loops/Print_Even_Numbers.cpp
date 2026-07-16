94acf7872b5b8435a671ea2c38d4669c5a43878e
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-16 23:47:05 +0530

    Create Print_Even_Numbers.cpp

---
+++ Loops/Print_Even_Numbers.cpp
@@ -0,0 +1,14 @@
+// Question: Write a C++ program to print even numbers from 1 to 100.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 2; i <= 100; i += 2)
+    {
+        cout << i << " ";
+    }
+
+    return 0;
+}
