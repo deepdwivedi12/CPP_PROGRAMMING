dd69619350006c30605145e8dbf3988fe48e8d06
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-20 22:08:12 +0530

    Add Floyd's Triangle program in C++
    This program prompts the user for the number of rows and prints Floyd's Triangle using nested loops.

---
+++ Patterns/Floyd's_Triangle.cpp
@@ -0,0 +1,25 @@
+// Question: Write a C++ program to print Floyd's Triangle.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, num = 1;
+
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            cout << num << " ";
+            num++;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
