2e24ed76c5faf924e462391880da8d7afc16de3a
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-20 22:00:35 +0530

    Create Number_Triangle.cpp

---
+++ Patterns/Number_Triangle.cpp
@@ -0,0 +1,24 @@
+// Question: Write a C++ program to print a number triangle pattern.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter size: ";
+    cin >> n;
+
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
