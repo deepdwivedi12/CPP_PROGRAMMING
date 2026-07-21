d91a85e872712abdcfd1dc901afcc275e1af1f2c
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-21 21:56:24 +0530

    Create Sum_of_two_numbers.cpp

---
+++ Function/Sum_of_two_numbers.cpp
@@ -0,0 +1,15 @@
+// Write a C++ program to create a function that prints the sum of two numbers.
+#include <iostream>
+using namespace std;
+
+void sum(int a, int b)
+{
+    cout << "Sum = " << a + b;
+}
+
+int main()
+{
+    sum(10, 20);
+
+    return 0;
+}
