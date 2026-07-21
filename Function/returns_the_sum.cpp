42d5980ee6d8ffc656a7af8968e89dbd116f893c
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-21 21:59:42 +0530

    Create returns_the_sum.cpp

---
+++ Function/returns_the_sum.cpp
@@ -0,0 +1,15 @@
+// Write a C++ program to create a function that returns the sum of two numbers.
+#include <iostream>
+using namespace std;
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    cout << "Sum = " << sum(10, 20);
+
+    return 0;
+}
