dc62e2b0813e37c4b0489999d35176078cb5763b
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-21 21:53:23 +0530

    Create Hello_World.cpp

---
+++ Function/Hello_World.cpp
@@ -0,0 +1,15 @@
+// Write a C++ program to create a function that prints "Hello World".
+#include <iostream>
+using namespace std;
+
+void hello()
+{
+    cout << "Hello World";
+}
+
+int main()
+{
+    hello();
+
+    return 0;
+}
