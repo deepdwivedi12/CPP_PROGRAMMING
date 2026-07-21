93a3f098f1cd9df1260b00ccc4e4fd302daa2e7c
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-21 21:58:26 +0530

    Create return_int.cpp

---
+++ Function/return_int.cpp
@@ -0,0 +1,15 @@
+// Write a C++ program to create a function that returns an integer value.
+#include <iostream>
+using namespace std;
+
+int number()
+{
+    return 100;
+}
+
+int main()
+{
+    cout << number();
+
+    return 0;
+}
