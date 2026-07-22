07081d191f255a8430ee428083cda4207507cc6c
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-22 22:12:37 +0530

    Create Swap_Two_Numbers.cpp

---
+++ Function/Swap_Two_Numbers.cpp
@@ -0,0 +1,30 @@
+// Write a C++ program to swap two numbers using a function.
+
+#include <iostream>
+using namespace std;
+
+
+void swapNumbers(int a, int b){
+    int temp = a;
+    a = b;
+    b = temp;
+
+    cout << "After Swapping:" << endl;
+    cout << "x = " << a << endl;
+    cout << "y = " << b;
+}
+
+int main()
+{
+    int x,y;
+
+    cout << "Enter x: ";
+    cin>>x;
+    cout << "Enter y: ";
+    cin >>y;
+
+    swapNumbers(x, y);
+
+
+    return 0;
+}
