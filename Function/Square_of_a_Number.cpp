1e1828e5fbe1feec53ac7a4121b57157f4e047e9
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-22 22:14:35 +0530

    Create Square_of_a_Number.cpp

---
+++ Function/Square_of_a_Number.cpp
@@ -0,0 +1,21 @@
+// Question: Write a C++ program to find the square of a number using a function.
+
+#include <iostream>
+using namespace std;
+
+int square(int n)
+{
+    return n * n;
+}
+
+int main()
+{
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout << "Square = " << square(num);
+
+    return 0;
+}
