90d9820d63c2fdc1d3e74d47d9e6dd0d42638e66
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-23 21:56:07 +0530

    Create Input_and_Display.cpp

---
+++ 1D Arrays/Input_and_Display.cpp
@@ -0,0 +1,25 @@
+// Question: Write a C++ program to input and display array elements.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5];
+
+    cout << "Enter 5 elements: ";
+
+    for(int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Array Elements: ";
+
+    for(int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
