2c8f7ec89b35a5b445b21236148b2bdfaf32ff62
Deep Dwivedi <deepdwivedi164@gmail.com>
2026-07-26 22:30:11 +0530

    Create Reverse_a_String.cpp

---
+++ Strings/Reverse_a_String.cpp
@@ -0,0 +1,21 @@
+// Question: Write a C++ program to reverse a string.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str;
+
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    cout << "Reversed String = ";
+
+    for(int i = str.length() - 1; i >= 0; i--)
+    {
+        cout << str[i];
+    }
+
+    return 0;
+}
